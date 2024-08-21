328b79f1ec3a797301b9fa4e87a4730bfad01a57
Jones Michael Fordjour <jonesmichaelfordjour2@gmail.com>
2024-08-21 02:23:19 +0000

    commit

--- main.py
+++ main.py
@@ -123,13 +123,99 @@
 # else:
 #     print('Odd')
 
-list1 = ['Giscard', 3, 'Michael', 'infinity']
+# list1 = ['Giscard', 3, 'Michael', 'infinity']
 
-list2 = ['Jones', 9, 'Fordjour', 'infinity']
+# list2 = ['Jones', 9, 'Fordjour', 'infinity']
 
-compare = 'infinity' in list2
+# compare = 'infinity' in list2
 
-if compare:
-    print('It is in')
-else:
-    print('It is not in')
+# if compare:
+#     print('It is in')
+# else:
+#     print('It is not in')
+
+# name = input('who? ')
+
+# match name:
+#     case 'Harry' | 'Fordjour' | 'Jones':
+#         print('I know him')
+#     case 'Justice' | 'Nti':
+#         print('Dont remember ')
+#     case _:
+#         print('Dont get you')
+
+# i = 3
+
+# while i != 0:
+#     print('meow')
+#     i -= 1
+
+# i = 0
+
+# while i < 3:
+#     print('hello')
+#     i = i + 1
+
+# for i in range(3):
+#     print('hello')
+
+# for _ in [1, 2, 4]:
+#     print('hello')
+# number = int(input('What is n '))
+
+# while True:
+#     if number > 2:
+#         break
+    
+
+
+# for _ in range(number):
+#     print('hello')
+
+# def main():
+#     number = get_number()
+#     meow(number)
+
+# def get_number():
+#     while True:
+#         n = int(input("What's n"))
+#         if n > 0:
+#             return n
+        
+# def meow(n):
+#     for _ in range(n):
+#         print('hello')
+
+
+# for student in students:
+#     print(student)
+# To use string in range() we use len(x)
+# students = ['Francis','Jones','Fordjour']
+
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+# Dictionary in python {'key': 'value'} pair
+
+# students = {
+#     'Jones': 'House 6',
+#     'Michael': 'House 5',
+#     'Fordjour': 'House 3',
+#     'Gerald': 'House 1'
+# }
+
+# print(students['Jones'])
+# print(students['Michael'])
+# print(students['Gerald'])
+
+# for student in students:
+#     print(student, students[student], sep=", ")
+
+students = [
+    {'name': 'Jones', 'house': 'house 2', 'section': '3'},
+    {'name': 'Michael', 'house': 'house 4', 'section': '1'},
+    {'name': 'Fordjour', 'house': 'house 5', 'section': None}
+]
+
+for student in students:
+    print(student['name'], student['house'])
